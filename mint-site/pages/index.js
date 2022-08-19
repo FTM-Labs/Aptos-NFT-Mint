8a1486bb446cf48b87585138d8dc1319bec50ed8
augustzms <minsheng@glitterprotocol.io>
2022-08-19 05:00:53 +0000

    Add mint tokens capability.

--- mint-site/pages/index.js
+++ mint-site/pages/index.js
@@ -23,29 +23,15 @@ export default function Home() {
 
   const mint = async () => {
     // Generate a transaction
-    var collection_name = "Rekt Cats"
-    var name = "test"
-    var description = "des"
-    var supply = 1
-    var uri = ""
+    var collection_name = "Test collection."
     const payload = {
       type: "script_function_payload",
-      function: "0xAECDE6ADBA10C2996AD0D8A18513404876C87C11924529A34D672195846EA3F1::candy_machine::create_token",
+      function: "0xFE1B69789117820C8CB68811E453FAB0FE0C67CE7E25EBF51DEEB7EE9083BD50::candy_machine_v2::mint_tokens",
       type_arguments: [],
       arguments: [
-          collection_name.encode("utf-8").hex(),
-          name.encode("utf-8").hex(),
-          description.encode("utf-8").hex(),
-          str(supply),
-          str(U64_MAX),
-          uri.encode("utf-8").hex(),
-          account.address(),
-          str(0),
-          str(0),
-          mutate_setting,
-          ["GiftTo".encode("utf-8").hex()],
-          ["Bob".encode("utf-8").hex()],
-          ["string".encode("utf-8").hex()]
+      	"0x16516ed571cb90ca28cf8361663ba7fab155b005b5eb6326c9a74373af5ae253",
+	"Test collection.",
+	"5",
       ]
     };
     const transaction = await window.martian.generateTransaction(sender, payload);
