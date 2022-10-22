0ab40749ffb9978414dd4ceed8d6737d044a6d22
AptosApe <175928134@qq.com>
2022-10-22 00:15:48 -0700

    change collection name

--- mint-site/helpers/candyMachineInfo.js
+++ mint-site/helpers/candyMachineInfo.js
@@ -1,6 +1,6 @@
 export const candyMachineAddress = "0x910d29c9605d6369edd2f895d4b93e1742819a63cf6c4535e3682701265f3149";
-export const collectionName = "Wolf of Aptos test"; // Case sensitive!
-export const collectionCoverUrl = "https://cloudflare-ipfs.com/ipfs/QmPJXqjBKPJq15ZQBqn6ZrcFRb546emUYSnpkXKjCXZPxy";
+export const collectionName = "Test collection"; // Case sensitive!
+export const collectionCoverUrl = "https://cloudflare-ipfs.com/ipfs//QmVsudH4sHELLHfaw3mYsX55zMXa2vpPAEvPsdPAV9Kwfx";
 export const mode = "dev"; // "dev" or "test" or "mainnet"
 
 export let NODE_URL;
