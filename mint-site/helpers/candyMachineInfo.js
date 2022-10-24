bbbfd7430f2a6a98aee078b09119950004225a4a
tolgaand <tolgaand@yandex.com>
2022-10-24 17:46:49 +0300

    Move environment variables from js file to .env file

--- mint-site/helpers/candyMachineInfo.js
+++ mint-site/helpers/candyMachineInfo.js
@@ -1,17 +1,18 @@
-export const candyMachineAddress = "0x910d29c9605d6369edd2f895d4b93e1742819a63cf6c4535e3682701265f3149";
-export const collectionName = "Test collection"; // Case sensitive!
-export const collectionCoverUrl = "https://cloudflare-ipfs.com/ipfs//QmVsudH4sHELLHfaw3mYsX55zMXa2vpPAEvPsdPAV9Kwfx";
-export const mode = "dev"; // "dev" or "test" or "mainnet"
+export const candyMachineAddress = process.env.NEXT_PUBLIC_CANDY_MACHINE_ID;
+export const collectionName = process.env.NEXT_PUBLIC_COLLECTION_NAME;
+export const collectionCoverUrl = process.env.NEXT_PUBLIC_COLLECTION_IMAGE_URI;
+export const mode = process.env.NEXT_PUBLIC_APTOS_NETWORK;
 
 export let NODE_URL;
 let FAUCET_URL;
-if (mode == "dev") {
-    NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
-    FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
+
+if (mode === "dev") {
+  NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
+  FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
 } else if (mode === "test") {
-    NODE_URL = "https://fullnode.testnet.aptoslabs.com/v1";
-    FAUCET_URL = "https://faucet.testnet.aptoslabs.com";
+  NODE_URL = "https://fullnode.testnet.aptoslabs.com/v1";
+  FAUCET_URL = "https://faucet.testnet.aptoslabs.com";
 } else {
-    NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
-    FAUCET_URL = null;
+  NODE_URL = "https://fullnode.mainnet.aptoslabs.com/v1";
+  FAUCET_URL = null;
 }
