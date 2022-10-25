34b365b4aceeda9c32683f124b1a31f4cde40259
Harvey <zzjiang@bu.edu>
2022-10-24 17:36:59 -0700

    update contract

--- mint-site/helpers/candyMachineInfo.js
+++ mint-site/helpers/candyMachineInfo.js
@@ -1,10 +1,10 @@
-export const candyMachineAddress = "0x6390bcd78627597e7b431e74b7c0a7ef8a489a20bdd3ea164e10440c6deff97b";
-export const collectionName = "Test"; // Case sensitive!
+export const candyMachineAddress = "0x3b3a0901cfc91b289cd16b62b28fd850e52a2921d463945a3722c33a81bad453";
+export const collectionName = "TestCollection101"; // Case sensitive!
 export const collectionCoverUrl = "https://cloudflare-ipfs.com/ipfs/QmeCuKGLvAGrBAPGdaRh2hkTjXsXuk8VEUHNoYRD4cJG51";
-export const mode = "test"; // "dev" or "test" or "mainnet"
+export const mode = "dev"; // "dev" or "test" or "mainnet"
 
 export let NODE_URL;
-export const CONTRACT_ADDRESS = "0x8c77a08888a537c39449b9e2e33cab520f0427acadc373bb569239cb2f22676d";
+export const CONTRACT_ADDRESS = "0xdf5c814388f4162f353e14f6123fcba8f39a958e4a2640e38e9e2c7cdfd2ac1d";
 export const COLLECTION_SIZE = 10
 let FAUCET_URL;
 if (mode == "dev") {
--- script/src/constants.py
+++ script/src/constants.py
@@ -12,7 +12,7 @@ else:
     FAUCET_URL=None
 
 
-CONTRACT_ADDRESS = "0xc071ef709539f7f9372f16050bf984fe6f11850594b8394f11bc74d22f48836b::candy_machine_v2"
+CONTRACT_ADDRESS = "0xdf5c814388f4162f353e14f6123fcba8f39a958e4a2640e38e9e2c7cdfd2ac1d::candy_machine_v2"
 MAX_GAS = 200000
 GAS_UNIT = 100
 IPFS_GATEWAY = "https://cloudflare-ipfs.com/ipfs/"
